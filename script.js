207d63a05b611facaa3793f7a08cf2f9991877de
Shabbir <shabbirdummy@gmail.com>
2023-07-07 16:04:02 +0530

    colorgenertor

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const getColor = () => {
+    const randomNumber = Math.floor( Math.random() * 1677215 )
+    const randomCode = "#" + randomNumber.toString(16)
+
+    document.body.style.backgroundColor = randomCode
+
+    document.getElementById("color-code").innerHTML = randomCode
+
+
+    navigator.clipboard.writeText(randomCode)
+}
+
+document.getElementById("btn").addEventListener("click" ,function(){ 
+    getColor()  
+}
+
+    )
+
+getColor()
